06fe39ac0fc7c6aefde939d97e181aff7e64384b
gaiagiulio <s296267@studenti.polito.it>
2025-06-30 19:57:31 +0200

    piccolo change

--- Sim_state.m
+++ Sim_state.m
@@ -17,7 +17,8 @@ LostClients_all = zeros(numQueue, n_sim);
 % Creating Config object with the configuration of the problem
 configuration = Config(StopNumber,numQueue, numServers);
 configuration.assignTimes({{'nonhomogeneous_poisson',  @(t) 1 + sin(t)}},{{'state.dipendent', @(state) 1/state}});
-configuration.assignBalking([1],min_balking(1), max_length(1));
+%configuration.assignBalking([1],min_balking(1), max_length(1));
+configuration.assignBalking([1],max_length(1), max_length(1));
 
 EventMgr= EventManager(configuration); % Creating Event Manager
 StatMgr= StatisticsManager(configuration.numQueue); % Creating Statistics Manager
