33976c82344ec77d2a6decaf74e31d6e728a557f
alessia <s295159@studenti.polito.it>
2025-07-02 15:33:29 +0200

    Aggiunta Simulazione MM1

--- MM1_Balking_Simulation.m
+++ MM1_Balking_Simulation.m
@@ -1,27 +1,3 @@
-% % SIMULATION EXAMPLE: M/M/1 queue with balking
-% 
-% % DA METTERE I RISULTATI FINALI E FARE LA SIMUAZIONE CON RIPETIZIONI
-% 
-% StopNumber = 200;
-% numQueue= 1;
-% numServers= [1];
-% min_balking=10;
-% max_length=15;
-% arrivalRate=3;
-% serviceRate=4;
-% 
-% % Creating Config object with the configuration of the problem
-% configuration = Config(StopNumber,numQueue, numServers);
-% configuration.assignTimes({{'iid', 'exponential',arrivalRate}},{{'iid', 'exponential',serviceRate}});
-% configuration.assignBalking([1],min_balking, max_length);
-% 
-% EventMgr= EventManager(configuration); % Creating Event Manager
-% StatMgr= StatisticsManager(configuration.numQueue); % Creating Statistics Manager
-% SimMgr=SimulationManager(StatMgr, EventMgr); % Creating Simulation Manager
-% 
-% SimMgr.SimulateEvents(configuration);
-% 
-% 
 % SIMULATION EXAMPLE: M/M/1 queue with balking and multiple repetitions
 
 StopNumber = 200;
